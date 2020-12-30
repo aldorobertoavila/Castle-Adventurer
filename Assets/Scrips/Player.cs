919ba460f1e5d757ac5d82de5557728598d4e311
Aldo Ávila <45251738+aldorobertoavila@users.noreply.github.com>
2020-12-30 11:18:38 -0800

    Add: Player Idle, Run, Crouch animations.
    Add: InputSystem package.
    Add: Player Input for Running, Flip, and Crouch actions.

---
+++ Assets/Scrips/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    // Components
+    public Animator animator;
+
+    // Constants
+    public float speed = 4.5F;
+
+    // Variables
+    public Vector2 facing;
+    public Vector2 motion;
+
+    public void Crouch(InputAction.CallbackContext ctx)
+    {
+        animator.SetBool("Crouching", !ctx.canceled);
+    }
+
+    public void Flip(InputAction.CallbackContext ctx)
+    {
+        float direction = ctx.ReadValue<float>();
+
+        if (ctx.started)
+            transform.Rotate(0, this.facing.x != direction ? 180 : 0, 0);
+
+        if (!ctx.canceled)
+            this.facing = new Vector2(direction, 0);
+
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        this.motion = ctx.ReadValue<Vector2>();
+    }
+
+    public void Run(InputAction.CallbackContext ctx)
+    {
+        animator.SetBool("Running", !ctx.canceled);
+
+        this.Move(ctx);
+    }
+
+    void Start()
+    {
+        this.facing = new Vector2(1, 0);
+    }
+
+    void FixedUpdate()
+    {
+        if(!this.animator.GetBool("Crouching"))
+            transform.Translate(this.motion * Time.fixedDeltaTime * this.speed, Space.World);
+    }
+
+}
